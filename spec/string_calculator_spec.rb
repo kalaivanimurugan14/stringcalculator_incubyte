d58c8a5491f139506873ccb50588f66296b94373
Kalaivani M <kalaivanimurugan14@gmail.com>
2024-08-30 11:37:53 +0530

    Files created and one test case added

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require_relative '../string_calculator'
+
+RSpec.describe StringCalculator do
+  describe '.add' do
+    
+    context 'when the input is an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.add('')).to eq(0)
+      end
+    end
+
+  end
+end
